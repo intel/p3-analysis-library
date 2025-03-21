e71b68ef7aff0ffc28da0a61d4be53f72fa189ae
John Pennycook <john.pennycook@intel.com>
2025-03-21 11:43:24 +0000

    Remove versioned coverage schema
    The library only supports a single version of the coverage schema,
    and there is no reason for us to maintain this list of previous versions.
    
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- p3analysis/data/_validation.py
+++ p3analysis/data/_validation.py
@@ -34,7 +34,7 @@ def _validate_coverage_json(json_string: str) -> object:
 
     instance = json.loads(json_string)
 
-    schema_string = pkgutil.get_data(__name__, "coverage-0.3.0.schema")
+    schema_string = pkgutil.get_data(__name__, "coverage.schema")
     if not schema_string:
         msg = "Could not locate coverage schema file"
         raise RuntimeError(msg)
@@ -47,7 +47,7 @@ def _validate_coverage_json(json_string: str) -> object:
         msg = "Coverage string failed schema validation"
         raise ValueError(msg)
     except jsonschema.exceptions.SchemaError:
-        msg = "coverage-0.3.0.schema is not a valid schema"
+        msg = "coverage.schema is not a valid schema"
         raise RuntimeError(msg)
 
     return instance
